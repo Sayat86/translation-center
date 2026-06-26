f1dd8cb6a8fc9f7e78ed10a6b264d5ae68d570fb
Sayat86 <s.beysembayev@bk.ru>
2026-06-26 10:31:44 +0500

    correct form.js

--- js/form.js
+++ js/form.js
@@ -1,21 +1,11 @@
-console.log("form.js loaded");
+
 const form = document.getElementById("contactForm");
 
-console.log(form);
-
 if (form) {
-
-    console.log("Форма найдена");
 
     form.addEventListener("submit", async function (e) {
 
         e.preventDefault();
-
-        console.log("Нажали отправить");
-
-        const formData = new FormData(form);
-
-        console.log([...formData.entries()]);
 
         const button = document.getElementById("submitBtn");
         const success = document.getElementById("successMessage");
@@ -26,7 +16,7 @@ if (form) {
         button.textContent = "Отправка...";
 
         const formData = new FormData(form);
-        formData.append("page", window.location.href);
+        formData.append("page", window.location.origin + window.location.pathname);
 
         try {
 
@@ -46,7 +36,12 @@ if (form) {
                 form.reset();
 
                 success.style.display = "block";
-                success.innerHTML = "✅ Спасибо! Заявка успешно отправлена.";
+                success.textContent = "✅ Спасибо! Заявка успешно отправлена.";
+
+                setTimeout(() => {
+                    success.style.display = "none";
+                    success.textContent = "";
+                }, 5000);
 
             } else {
 
@@ -57,7 +52,6 @@ if (form) {
         } catch (error) {
 
             alert("Ошибка соединения с сервером.");
-
             console.error(error);
 
         }
